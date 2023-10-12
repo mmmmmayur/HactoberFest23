3a2b1bb4d7713089806c55c7ba0f06d8539697fb
Mayur Salunkhe <121350999+mmmmmayur@users.noreply.github.com>
2023-10-13 02:50:33 +0530

    Create doublylinkedlist.java

---
+++ doublylinkedlist.java
@@ -0,0 +1,65 @@
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+public class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+
+    public DoublyLinkedList() {
+        head = null;
+        tail = null;
+    }
+
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    public void displayForward() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " <-> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public void displayBackward() {
+        Node current = tail;
+        while (current != null) {
+            System.out.print(current.data + " <-> ");
+            current = current.prev;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.append(1);
+        dll.append(2);
+        dll.append(3);
+        dll.append(4);
+
+        System.out.println("Forward Traversal:");
+        dll.displayForward();
+
+        System.out.println("Backward Traversal:");
+        dll.displayBackward();
+    }
+}
